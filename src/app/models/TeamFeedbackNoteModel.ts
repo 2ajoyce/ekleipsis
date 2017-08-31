c77c07ae399e9ac7ec8606953b4bc545b8053072
Platypus <platypus@Platypuss-iMac.local>
2017-08-30 23:07:47 -0500

    Changed note model

--- src/app/models/TeamFeedbackNoteModel.ts
+++ src/app/models/TeamFeedbackNoteModel.ts
@@ -2,10 +2,10 @@ import {FeedbackCategory} from './feedback-category.enum';
 import {User} from './user';
 
 export class TeamFeedbackNote {
-    public TeamFeedbackNoteId: number;
-    public Employee: User;
-    public Category: FeedbackCategory;
-    public Text: string;
-    public Sender?: User;
-    public IsAnonymous: boolean;
+    public noteId: number;
+    public employee: User;
+    public category: FeedbackCategory;
+    public message: string;
+    public sender?: User;
+    public isAnonymous: boolean;
 }
