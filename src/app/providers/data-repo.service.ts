fec04beb64671e94f873e0a59480697d7c92cfe9
Joyce <AJOYCE@corp.vha.ad>
2017-08-30 19:33:38 -0500

    WIP data repo

--- src/app/providers/data-repo.service.ts
+++ src/app/providers/data-repo.service.ts
@@ -7,26 +7,54 @@ import * as firebase from 'firebase/app';
 
 @Injectable()
 export class DataRepoService {
-  user: Observable<firebase.User>;
-  db: FirebaseObjectObservable<any[]>;
-  items: FirebaseListObservable<any[]>;
-  teamMembersFromFB: FirebaseListObservable<any[]>;
-  isAdding: boolean = false;
-  addData: '';
-  editData: '';
-  editingKey: string = '';
-  teamMembers: Array<TeamMember> = [];
-  teamFeedback: boolean = true;
-  teamFeedbackNotesFromFB: FirebaseListObservable<any[]>;
+ user: Observable<firebase.User>;
+ db: FirebaseObjectObservable<any[]>;
+ items: FirebaseListObservable<any[]>;
+ teamMembersFromFB: FirebaseListObservable<any[]>;
+ isAdding: boolean = false;
+ addData: '';
+ editData: '';
+ editingKey: string = '';
+ teamMembers: Array<TeamMember> = [];
+ teamFeedback: boolean = true;
+ teamFeedbackNotesFromFB: FirebaseListObservable<any[]>;
 
 
-  constructor(afAuth: AngularFireAuth, public af: AngularFireDatabase) {
-    this.user = afAuth.authState;
-    this.db = af.object('/');
-    this.items = af.list('/items');
-    this.teamMembersFromFB = af.list('/teams/ekleipsis/members');
-    this.teamFeedbackNotesFromFB = af.list('/teamFeedbackNotes/0');
-  }
+ constructor(afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+ this.user = afAuth.authState;
+ this.db = af.object('/');
+ this.items = af.list('/items');
+ this.teamMembersFromFB = af.list('/teams/ekleipsis/members');
+ this.teamFeedbackNotesFromFB = af.list('/teamFeedbackNotes/0');
+ }
+
+ public getFeedbackNotes(): FirebaseListObservable {
+
+ }
+
+ public getOneOnOneNotes(): FirebaseListObservable {
+
+ }
+
+ public getTeamMembers(): FirebaseListObservable {
+
+ }
+
+ public getSubordinates(): FirebaseListObservable {
+
+ }
+
+ public setFeedbackNotes(): FirebaseListObservable {
+
+ }
+
+ public setOneOnOneNotes(): FirebaseListObservable {
+
+ }
+
+ public setSubordinates(): FirebaseListObservable {
+
+ }
 
 
 }
