9b459d469a99413cad428f534462043061088f21
Platypus <platypus@Platypuss-iMac.local>
2017-08-30 23:02:14 -0500

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import {TeamMember} from './models/TeamMember';
 import {Observable} from 'rxjs/Observable';
 import {AngularFireAuth} from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import {DataRepoService} from './providers/data-repo.service';
+import {User} from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +13,10 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  promise:any;
   user: Observable<firebase.User>;
   db: FirebaseObjectObservable<any[]>;
-  items: FirebaseListObservable<any[]>;
+  users: User[];
   teamMembersFromFB: FirebaseListObservable<any[]>;
   isAdding: boolean = false;
   addData: '';
@@ -24,6 +27,7 @@ export class AppComponent {
   teamMembers: string[] = [];
   activePosition: string = '';
   activeTab: string = 'teamFeedback';
+  repoService: DataRepoService;
 
   switchTab(tab: string) {
     this.activeTab = tab;
@@ -32,7 +36,13 @@ export class AppComponent {
   constructor(afAuth: AngularFireAuth, public af: AngularFireDatabase) {
     this.user = afAuth.authState;
     this.db = af.object('/');
-    this.items = af.list('/items');
+    // this.promise = af.list('/users').$ref.once('value', function (snap) {
+    //   snap.forEach(function (value) {
+    //     console.log('user', value.val());
+    //     return true;
+    //   });
+    // });
+
     this.teamMembersFromFB = af.list('/teams/ekleipsis/members');
     this.teamFeedbackNotesFromFB = af.list('/teamFeedbackNotes/0');
     this.teamMembers.push('Joseph Bartley');
@@ -41,6 +51,8 @@ export class AppComponent {
     this.teamMembers.push('Brian Giunta');
     this.teamMembers.push('Kyle Shaffar');
     this.activePosition = 'Associate Software Engineer, AD';
+    this.repoService = new DataRepoService(afAuth, af);
+    this.users = this.repoService.getUsers();
   }
 
   // Runs on init of the page
